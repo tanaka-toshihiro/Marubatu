13084df420b2b3880390fe852e80a1bda2ae8ad1
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-02-27 19:33:55 +0900

    Update Marubatu.java

--- Marubatu.java
+++ Marubatu.java
@@ -1,71 +1,65 @@
 import java.util.Scanner;
 public class Marubatu{
-   public static void main(String[] args){
-      int win=1;
-      int size = 3;
-      int[][]board = new  int[size][size];//
+    public static void main(String[] args){
+    int win=1;
+    int size = 3;
+    int[][]board = new  int[size][size];
 
-       for(int i = 0; i < size; i++ ){
-          for(int j = 0; j < size; j++ ){
-            board[i][j] =  50;//
-              
- 	      	if(j ==2){ 
-			System.out.println(" |");
-	       }else{
-			System.out.print(" |");
-           } 
-	    	}
-	   }
-	   
-	   String []koma = new String[101];
-	   koma[0] = "O";
-	   koma[50] = " "	;
-	   koma[100] = "X";  
-	  Scanner sc = new Scanner(System.in);
-	   for(int m = 0; m< size*size; m++){
-	        int turn = m%2+1;// 1 or 2
-			int player = m%2*100;// 0 or 100
-	        System.out.println(turn+" player");
-         
-		 int  tate; int yoko;
-		   System.out.println("tate");
+    for(int i = 0; i < size; i++ ){
+        for(int j = 0; j < size; j++ ){
+        board[i][j] =  0;
+		}
+    }
+    String []koma = new String[3];
+    koma[0] = " ";
+    koma[1] = "O";
+    koma[2] = "X";  
+    Scanner sc = new Scanner(System.in);
+
+    for(int m = 0; m< size*size; m++){
+        int turn = m%2+1;// 1 or 2
+        System.out.println(turn+" player");
+                
+        int  tate; int yoko;
+        do{
+            System.out.println("tate");
             tate  = sc.nextInt();
             System.out.println("yoko");
             yoko  = sc.nextInt();
-        
-       while(tate<0 || yoko < 0  || tate >= size || yoko >= size || board[tate][yoko]!= 50 );
-	     board [tate][yoko] = player;
-       
-  	     for(int i=0; i<size; i++){
-              String row = "";
-              for(int j=0; j<size; j++){
+            }while(tate<0 || yoko < 0  || tate >= size || yoko >= size || board[tate][yoko]!= 0 );
+	            board [tate][yoko] = turn;
+					
+                for(int i=0; i<size; i++){
+                    String row = "";
+                    for(int j=0; j<size; j++){
                     row+= koma[board[i][j]]+"|";
+                    }
+                    System.out.println(row);
                 }
-                System.out.println(row);
-           }
-		   
-		if(board[0][0] + board[0][1] + board[0][2] == 0 || board[1][0]+  board[1][1] + board[1][2] ==0 || board[2][0] + board[2][1] + board[2][2] == 0 
-		|| board[0][0] + board[1][0] + board[2][0] == 0 || board[0][1]+  board[1][1] + board[2][1] == 0 || board[0][2] + board[1][2] + board[2][2] == 0 
-		|| board[0][0] + board[1][1] + board[2][2] == 0 || board[0][2] + board[1][1] + board[2][0] == 0){
-		System.out.println("1 win");
-		break;	
-		}
-		if(board[0][0] + board[0][1] + board[0][2] ==300 ||  board[1][0] + board[1][1] + board[1][2] ==300  ||  board[2][0] + board[2][1] + board[2][2] ==300 
-		|| board[0][0] + board[1][0] + board[2][0] == 300 || board[0][1] +  board[1][1] + board[2][1] == 300 || board[0][2] + board[1][2] + board[2][2] == 300
-		|| board[0][0] + board[1][1] + board[2][2] ==300 || board[0][2] + board[1][1] + board[2][0] ==300){
-		System.out.println("2 win");
-		break;	
-	    }else if(board[0][0] != 50 && board[0][1] != 50 && board[0][2] != 50
-		&& board[1][0] != 50 && board[1][1] != 50 && board[1][2] != 50
-		&& board[2][0] != 50 && board[2][1] != 50 && board[2][2] != 50 ){
-		System.out.println("drow");
-        break;
-	    }
-	 }
-	 }
-	 }
+				
+                if(board[0][0] * board[0][1] * board[0][2] == 1 || board[1][0] *  board[1][1] * board[1][2] ==1 || board[2][0] * board[2][1] * board[2][2] == 1 
+                || board[0][0] * board[1][0] * board[2][0] == 1 || board[0][1] *  board[1][1] * board[2][1] == 1 || board[0][2] * board[1][2] * board[2][2] == 1 
+                || board[0][0] * board[1][1] * board[2][2] == 1 || board[0][2] * board[1][1] * board[2][0] == 1){
+                System.out.println("1  player wins");
+                break;	
+                }
+                if(board[0][0] * board[0][1] * board[0][2] ==8 ||  board[1][0] * board[1][1] * board[1][2] ==8  ||  board[2][0] * board[2][1] * board[2][2] ==8 
+                || board[0][0] * board[1][0] * board[2][0] == 8 || board[0][1] *  board[1][1] * board[2][1] == 8 || board[0][2] * board[1][2] * board[2][2] == 8
+                || board[0][0] * board[1][1] * board[2][2] ==8 || board[0][2] * board[1][1] * board[2][0] ==8){
+                System.out.println("2  player wins");
+                break;	
+                }else if(board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0
+                && board[1][0] != 0 && board[1][1] != 0 && board[1][2] != 0
+                && board[2][0] != 0 && board[2][1] != 0 && board[2][2] != 0 ){
+                System.out.println("draw");
+                break;
+                }
+        }
+        }
+ }
 	
 	  
+	
 			  
 			  	
 	 
