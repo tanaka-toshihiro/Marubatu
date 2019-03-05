a8364693eed3d3fc8f10d8d8f693e3e7c7e43f4b
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-03-05 23:28:41 +0900

    Update Marubatu.java

--- Marubatu.java
+++ Marubatu.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Marubatu{
   public static void main(String[] args){
     int size = 3;
-    int[][]board = new  int[size][size];
+    int[][] board = new  int[size][size];
   
     for(int i = 0; i < size; i++ ){
       for(int j = 0; j < size; j++ ){
